1ef4fee318cdebe900a241de4c7fe1d6cf31e5c1
NileshVS <50776400+NileshVS@users.noreply.github.com>
2019-11-12 21:51:52 +0530

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import {CourseDetails} from './courseDetails';
-import { OnClick } from './onClick.component';
-import { Parent } from './parent.component';
-import { Child } from './child.component';
-import {ReactiveFormComponent} from './reactive-form/reactive-form.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
-import { NgBasicsComponent } from './ng-basics/ng-basics.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import {RouterModule} from '@angular/router';
 import {appRoutes} from './app.routes';
-import { ContactComponent } from './contact/contact.component';
-import { CareerComponent } from './career/career.component';
 import { ProductComponent } from './product/product.component';
 import {HttpClientModule} from  '@angular/common/http';
-import { AddProductComponent } from './add-product/add-product.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OnClick,
-    Parent,
-    Child,
-    ReactiveFormComponent,
-    TemplateFormComponent,
-    NgBasicsComponent,
     NavbarComponent,
     HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    CareerComponent,
     ProductComponent,
-    AddProductComponent,
     LoginComponent
   ],
   imports: [
@@ -49,7 +27,7 @@ import { LoginComponent } from './login/login.component';
     HttpClientModule,
     ModalModule.forRoot()
   ],
-  providers: [CourseDetails],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
